c39a5be2603e1ce9c69efdcb8d2f299199fc948f
Mago <martingramajo08@gmail.com>
2025-02-12 10:47:57 -0300

    agrego las rutas privadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./views/Home";
-import Admin from "./views/Admin";
 import Login from "./views/Login";
+import { useState } from "react";
+import EncapsularRutas from "./routes/EncapsularRutas";
+import RutasProtegidas from "./routes/RutasProtegidas";
 
 
 const App = () => {
+
+  const usuarioEnLinea = JSON.parse(sessionStorage.getItem('usuarioLogueado')) || {};
+  const [usuarioActivo, setUsuarioActivo] = useState(usuarioEnLinea);
+  
   return (
     <div>
       <Router>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/admin" element={<Admin />} /> */}
+        <Route path="/login" element={<Login setUsuarioActivo={setUsuarioActivo}></Login>} />
+        <Route
+          path="/admin/*"
+          element={
+          <EncapsularRutas>
+            <RutasProtegidas></RutasProtegidas>
+          </EncapsularRutas>}
+        ></Route>
       </Routes>
     </Router>
       
